8283b149d33fd23e58335742922736b8e2d28565
Jason Doyley <codemonk_@outlook.com>
2019-09-02 18:45:45 -0400

    [R5D4]: Add elements to the DOM using React

--- intro/exercise/01-hello-world.js
+++ intro/exercise/01-hello-world.js
@@ -1,7 +1,17 @@
 // PURE JAVASCRIPT
 
+// const rootEl = document.getElementById('root');
+// const element = document.createElement('h1');
+// element.textContent = 'Hello, World!';
+// element.className = 'container';
+// rootEl.appendChild(element);
+
+// REACT
 const rootEl = document.getElementById('root');
-const element = document.createElement('h1');
-element.textContent = 'Hello, World!';
-element.className = 'container';
-rootEl.appendChild(element);
+const element = React.createElement(
+  'h1',
+  { className: 'container' },
+  'Hello, World!'
+);
+
+ReactDOM.render(element, rootEl);
