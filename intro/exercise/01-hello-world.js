b51816c04d163da868485e54e1d42065a81dd1ea
Jason Doyley <codemonk_@outlook.com>
2019-09-02 18:41:55 -0400

    [R5D4] Add elements to the DOM using JavaScript

---
+++ intro/exercise/01-hello-world.js
@@ -0,0 +1,7 @@
+// PURE JAVASCRIPT
+
+const rootEl = document.getElementById('root');
+const element = document.createElement('h1');
+element.textContent = 'Hello, World!';
+element.className = 'container';
+rootEl.appendChild(element);
