665742ed86ec02309e6914726fd85b13d9a6f876
Jason Doyley <codemonk_@outlook.com>
2019-09-03 06:03:35 -0400

    [R5D5]: Replace React createElement Function Call with JSX

--- intro/exercise/02-jsx.js
+++ intro/exercise/02-jsx.js
@@ -4,6 +4,6 @@ const props = {
   className: 'container',
   children: 'Hello, World!'
 };
-const element = <h1 {...props} />
+const element = <h1 {...props} className="my-class" />
 
 ReactDOM.render(element, rootEl);
